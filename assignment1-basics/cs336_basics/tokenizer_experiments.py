9b532a8f6a2520ef219c92cfb97fa17b400b9eb4
irelandolgpig <xuweiweng2002@gmail.com>
2025-07-12 03:25:36 +0800

    optimize tokenizer

--- assignment1-basics/cs336_basics/tokenizer_experiments.py
+++ assignment1-basics/cs336_basics/tokenizer_experiments.py
@@ -1,5 +1,17 @@
 import os
+import tracemalloc
 from cs336_basics.tokenizer import Tokenizer
+import time
+
+def timing(f):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = f(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{f.__name__} time: {end - start:.6f}s")
+        return result
+    return wrapper
+
 
 def analyze_tokenizer(tokenizer: Tokenizer, top_k: int = 5) -> None:
     """
@@ -8,6 +20,7 @@ def analyze_tokenizer(tokenizer: Tokenizer, top_k: int = 5) -> None:
     - Top-K longest tokens
     - Vocab and merge sizes
     """
+    print("=" * 50)
     print(f"📦 Total tokenizer.vocab size: {len(tokenizer.vocab)}")
     print(f"🔗 Total tokenizer.merges: {len(tokenizer.merges)}\n")
 
@@ -21,7 +34,9 @@ def analyze_tokenizer(tokenizer: Tokenizer, top_k: int = 5) -> None:
         except UnicodeDecodeError:
             decoded = "<invalid utf-8>"
         print(f" - ID {idx}: {token} (len={len(token)} bytes) → {decoded}")
+    print("=" * 50)
 
+@timing
 def validate_tokenizer(tokenizer: Tokenizer, valid_path: str):
     for i in range(len(tokenizer.merges)):
         assert tokenizer.merges[i][0] + tokenizer.merges[i][1] == tokenizer.vocab[i + 257], f"i = {i}, tokenizer.merges[{i}]: {tokenizer.merges[i]}, tokenizer.vocab: {tokenizer.vocab[i + 257]}"
@@ -31,10 +46,18 @@ def validate_tokenizer(tokenizer: Tokenizer, valid_path: str):
     assert data == tokenizer.decode(tokenizer.encode(data))
 
 if __name__ == "__main__":
-    read_path = os.path.join("tokenizers", "tokenizer_readable-TinyStories_train-v10000.pkl")
-    valid_path = os.path.join("data", "TinyStoriesV2-GPT4-valid.txt")
-    tokenizer = Tokenizer(vocab={}, merges=[])
-    tokenizer.from_file(read_path, special_tokens=["<|endoftext|>"])
+    tinystories_read_path = os.path.join("tokenizers", "tokenizer_readable-TinyStories_train-v10000.pkl")
+    tinystories_valid_path = os.path.join("data", "TinyStoriesV2-GPT4-valid.txt")
+    tinystories_tokenizer = Tokenizer(vocab={}, merges=[])
+    tinystories_tokenizer.from_file(tinystories_read_path, special_tokens=["<|endoftext|>"])
+
+    openwebtext_read_path = os.path.join("tokenizers", "tokenizer_readable-owt_train-v32000.pkl")
+    openwebtext_valid_path = os.path.join("data", "owt_valid.txt")
+    openwebtext_tokenizer = Tokenizer(vocab={}, merges=[])
+    openwebtext_tokenizer.from_file(openwebtext_read_path, special_tokens=["<|endoftext|>"])
         
-    analyze_tokenizer(tokenizer, top_k=5)
-    validate_tokenizer(tokenizer, valid_path)
+    # analyze_tokenizer(tinystories_tokenizer, top_k=5)
+    validate_tokenizer(tinystories_tokenizer, tinystories_valid_path)
+
+    # analyze_tokenizer(openwebtext_tokenizer, top_k=5)
+    validate_tokenizer(openwebtext_tokenizer, openwebtext_valid_path)
