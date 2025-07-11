30f0e591e525f99d388fada6195dbde7d3bcfa73
irelandolgpig <xuweiweng2002@gmail.com>
2025-07-12 04:26:08 +0800

    optimize tokenizer

--- cs336_basics/tokenizer_experiments.py
+++ cs336_basics/tokenizer_experiments.py
@@ -1,7 +1,7 @@
 import os
-import tracemalloc
 from cs336_basics.tokenizer import Tokenizer
 import time
+import numpy as np
 
 def timing(f):
     def wrapper(*args, **kwargs):
@@ -11,7 +11,6 @@ def timing(f):
         print(f"{f.__name__} time: {end - start:.6f}s")
         return result
     return wrapper
-
 
 def analyze_tokenizer(tokenizer: Tokenizer, top_k: int = 5) -> None:
     """
@@ -36,7 +35,6 @@ def analyze_tokenizer(tokenizer: Tokenizer, top_k: int = 5) -> None:
         print(f" - ID {idx}: {token} (len={len(token)} bytes) → {decoded}")
     print("=" * 50)
 
-@timing
 def validate_tokenizer(tokenizer: Tokenizer, valid_path: str):
     for i in range(len(tokenizer.merges)):
         assert tokenizer.merges[i][0] + tokenizer.merges[i][1] == tokenizer.vocab[i + 257], f"i = {i}, tokenizer.merges[{i}]: {tokenizer.merges[i]}, tokenizer.vocab: {tokenizer.vocab[i + 257]}"
@@ -45,19 +43,31 @@ def validate_tokenizer(tokenizer: Tokenizer, valid_path: str):
         data = f.read()
     assert data == tokenizer.decode(tokenizer.encode(data))
 
+def encode_data(tokenizer: Tokenizer, data_path: str, save_path: str):
+    token_ids = np.array(tokenizer.encode(open(data_path, encoding="utf-8").read()), dtype=np.uint16)
+    token_ids.tofile(save_path)
+
+
 if __name__ == "__main__":
     tinystories_read_path = os.path.join("tokenizers", "tokenizer_readable-TinyStories_train-v10000.pkl")
     tinystories_valid_path = os.path.join("data", "TinyStoriesV2-GPT4-valid.txt")
+    tinystories_data_path = os.path.join("data", "TinyStoriesV2-GPT4-train.txt")
+    tinystories_save_path = os.path.join("data", "TinyStoriesV2-GPT4-train.npy")
     tinystories_tokenizer = Tokenizer(vocab={}, merges=[])
     tinystories_tokenizer.from_file(tinystories_read_path, special_tokens=["<|endoftext|>"])
 
     openwebtext_read_path = os.path.join("tokenizers", "tokenizer_readable-owt_train-v32000.pkl")
     openwebtext_valid_path = os.path.join("data", "owt_valid.txt")
+    openwebtext_data_path = os.path.join("data", "owt_train.txt")
+    openwebtext_save_path = os.path.join("data", "owt_train.npy")
     openwebtext_tokenizer = Tokenizer(vocab={}, merges=[])
     openwebtext_tokenizer.from_file(openwebtext_read_path, special_tokens=["<|endoftext|>"])
         
     # analyze_tokenizer(tinystories_tokenizer, top_k=5)
-    validate_tokenizer(tinystories_tokenizer, tinystories_valid_path)
+    # validate_tokenizer(tinystories_tokenizer, tinystories_valid_path)
+    # encode_data(tinystories_tokenizer, tinystories_data_path, tinystories_save_path)
+
 
     # analyze_tokenizer(openwebtext_tokenizer, top_k=5)
-    validate_tokenizer(openwebtext_tokenizer, openwebtext_valid_path)
+    # validate_tokenizer(openwebtext_tokenizer, openwebtext_valid_path)
+    encode_data(openwebtext_tokenizer, openwebtext_data_path, openwebtext_save_path)
