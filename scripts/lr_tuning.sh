355646e73012b9550d51247805964602edbce0f9
irelandolgpig <xuweiweng2002@gmail.com>
2025-07-14 23:12:12 +0800

    implement wandb.sweep

--- scripts/lr_tuning.sh
+++ scripts/lr_tuning.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# 超参数列表
-# learning_rates=(1e-1 5e-2 1e-2 5e-3 1e-3)
-
-lr=1.5e-2
-warmup=1600
-batch_size=64
-
-echo "Training with learning_rate = $lr"
-
 uv run cs336_basics/training_together.py \
   --d_model 512 \
   --num_heads 16 \
@@ -17,21 +7,23 @@ uv run cs336_basics/training_together.py \
   --num_layers 4 \
   --context_length 256 \
   --theta 10000 \
-  --batch_size $batch_size \
-  --max_steps 20000 \
-  --lr_warmup_steps $warmup \
-  --lr_decay_steps 20000 \
-  --learning_rate $lr \
+  --device auto \
+  --dtype float32 \
+  --batch_size 64 \
   --optimizer adamw \
-  --train_data "data/TinyStoriesV2-GPT4-train.npy" \
-  --val_data "data/TinyStoriesV2-GPT4-valid.npy" \
-  --checkpoint_dir "checkpoints/TinyStories_LM/lr_${lr}" \
+  --lr_warmup_steps 1600 \
+  --lr_decay_steps 20000 \
+  --max_steps 20000 \
   --log_interval 10 \
   --eval_interval 10 \
   --save_interval 1000 \
-  --device auto \
-  --dtype float32 \
+  --checkpoint_dir "checkpoints/TinyStories_LM" \
+  --train_data "data/TinyStoriesV2-GPT4-train.npy" \
+  --val_data "data/TinyStoriesV2-GPT4-valid.npy" \
   --use_wandb \
   --wandb_project "CS336_assignment1" \
-  --wandb_run "lr:${lr} bs:${batch_size} warmup:${warmup}" \
+  --sweep \
+  --sweep_method grid \
+  --sweep_runs 5 \
+    --sweep_param "learning_rate,log_uniform_values,1e-5,1e-2" \ \
   "$@"
